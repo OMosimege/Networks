ae427ea78dbcb3bcb50987a805db00210ca9d74e
Cerojo <cecil.hlungwana@gmail.com>
2018-03-06 20:05:40 +0200

    Added tasks to be done for the application

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by cecil on 06 Mar 2018.
+ * I am using intellij IDEA 2017.1.2
+ *
+ * This is the main class in which our application will start.
+ * To test each class individually, you can use this class.
+ * The task will be spilt up into different classes.
+ * Some classes will be tightly coupled, we must find a way around that.
+ *
+ * Tasks
+ * - GUI
+ * - Server
+ * - Client
+ * - Driver
+ * - Other
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
--- src/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by cecil on 06 Mar 2018.
- */
-public class main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
